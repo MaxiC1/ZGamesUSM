7c995e4ac2a6aac65d31922588c5a3681398fc65
Maximiliano Casanova <maxi.casanova2205@gmail.com>
2021-06-19 17:12:15 -0400

    Generar interfaz de ver Consola y de Agregar Consola

--- web/zgames/resources/views/layouts/master.blade.php
+++ web/zgames/resources/views/layouts/master.blade.php
@@ -1,18 +1,21 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <head>
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="csrf-token" content="{{csrf_token()}}">
 
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+     <link rel="stylesheet" href="{{asset('css/style.css')}}">
      <title>Z Games USM</title>
   </head>
   <body>
       <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-warning">
             <div class="container-fluid">
-              <a class="navbar-brand" href="{{route('home')}}">Z Games</a>
+              <a class="navbar-brand" href="{{route('home')}}">
+                <img class="logo" src="{{asset('img/logo_z.png')}}">
+              </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -28,8 +31,12 @@
           </nav> 
       </header>
       <main class="container fluid">
-          @yield("contenido")
+        @yield("contenido")
       </main>
+
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-  </body>
+      <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+      <script src="{{asset('js/axios_config.js')}}"></script>
+
+    </body>
 </html>
